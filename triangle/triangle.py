1f1bc6ac9c422ef8247d72e39b4342b650934627
Egor Kostan <igorkostan@gmail.com>
2025-08-25 21:36:36 -0700

    Update triangle.py

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -5,7 +5,7 @@ def equilateral(sides: list) -> bool:
     """
     An equilateral triangle has all three sides the same length.
     """
-    if check_for_zeroes(sides) and inequality_violation(sides):
+    if all_sides_positive(sides) and no_inequality_violation(sides):
         return sides[0] == sides[1] == sides[2]
     return False
 
@@ -16,7 +16,7 @@ def isosceles(sides: list) -> bool:
     (It is sometimes specified as having exactly two sides the same length,
     but for the purposes of this exercise we'll say at least two.)
     """
-    if check_for_zeroes(sides) and inequality_violation(sides):
+    if all_sides_positive(sides) and no_inequality_violation(sides):
         return sides[0] == sides[1] or sides[1] == sides[2] or sides[0] == sides[2]
     return False
 
@@ -25,12 +25,12 @@ def scalene(sides: list) -> bool:
     """
     A scalene triangle has all sides of different lengths.
     """
-    if check_for_zeroes(sides) and inequality_violation(sides):
+    if all_sides_positive(sides) and no_inequality_violation(sides):
         return sides[0] != sides[1] and sides[1] != sides[2] and sides[0] != sides[2]
     return False
 
 
-def inequality_violation(sides: list) -> bool:
+def no_inequality_violation(sides: list) -> bool:
     """
     Let a, b, and c be sides of the triangle.
     Then all three of the following expressions must be true:
@@ -42,8 +42,9 @@ def inequality_violation(sides: list) -> bool:
     return sides[0] + sides[1] >= sides[2] and sides[0] + sides[2] >= sides[1] and sides[2] + sides[1] >= sides[0]
 
 
-def check_for_zeroes(sides: list) -> bool:
-    """No zeroes allowed."""
-    if 0 in sides:
-        return False
-    return True
+def all_sides_positive(sides: list) -> bool:
+    """
+    No zeroes or negative numbers allowed.
+    All triangles should have 3 sides.
+    """
+    return all(side > 0 for side in sides) and len(sides) == 3

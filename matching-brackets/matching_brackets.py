afb4d12b117557fe189a1aa6d7f339beb6f43c11
Egor Kostan <igorkostan@gmail.com>
2025-08-31 17:29:09 -0700

    First test

--- matching-brackets/matching_brackets.py
+++ matching-brackets/matching_brackets.py
@@ -1,2 +1,28 @@
-def is_paired(input_string):
+"""
+Matching Brackets.
+
+You're given the opportunity to write software for the Bracketeer™,
+an ancient but powerful mainframe. The software that runs on it is
+written in a proprietary language. Much of its syntax is familiar,
+but you notice lots of brackets, braces and parentheses. Despite
+the Bracketeer™ being powerful, it lacks flexibility. If the source
+code has any unbalanced brackets, braces or parentheses, the Bracketeer™
+crashes and must be rebooted. To avoid such a scenario, you start writing
+code that can verify that brackets, braces, and parentheses are balanced
+before attempting to run it on the Bracketeer™.
+"""
+
+
+def is_paired(input_string: str) -> bool:
+    """
+    Verify that any and all pairs of brackets are matched.
+
+    Given a string containing brackets [], braces {}, parentheses (),
+    or any combination thereof, verify that any and all pairs are matched
+    and nested correctly. Any other characters should be ignored. For example,
+    "{what is (42)}?" is balanced and "[text}" is not.
+
+    :param input_string:
+    :return:
+    """
     pass

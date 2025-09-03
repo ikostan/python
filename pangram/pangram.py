d4395fa4233bc172bf0bb54404245b266d2caf08
Egor Kostan <igorkostan@gmail.com>
2025-09-03 15:14:55 -0700

    Update pangram.py

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,18 +1,14 @@
 """
 Pangram.
 
-You work for a company that sells fonts through their website. They'd like to show a
-different sentence each time someone views a font on their website. To give a
-comprehensive sense of the font, the random sentences should use all the letters
-in the English alphabet.
+You work for a company that sells fonts through their website. They'd like to
+show a different sentence each time someone views a font on their website.
+To give a  comprehensive sense of the font, the random sentences should use
+all the letters in the English alphabet.
 
-They're running a competition to get suggestions for sentences that they can use.
-You're in charge of checking the submissions to see if they are valid.
+They're running a competition to get suggestions for sentences that they can
+use. You're in charge of checking the submissions to see if they are valid.
 """
-
-import string
-
-letters: str = string.ascii_lowercase
 
 
 def is_pangram(sentence: str) -> bool:
@@ -21,13 +17,13 @@ def is_pangram(sentence: str) -> bool:
 
     :param sentence: The sentence to check for pangram validity
     :type sentence: str
-    :return: True if the sentence contains all 26 letters of the English alphabet, False otherwise
+    :return: True if the sentence contains all 26 letters of the English alphabet,
+             False otherwise
     :rtype: bool
     """
     # Empty string
     if not sentence:
         return False
-
     # Convert sentence to lowercase and extract unique alphabetic characters
     unique_letters: set = set(
         char for char in sentence.lower() if char.isalpha()
